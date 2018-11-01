e474213a3f9760af933989a1b33a1390e31dfee0
Unknown <19parfrg@elmbrookstudents.org>
2018-11-01 13:51:07 -0500

    stuff

--- src/GrantsClass.java
+++ src/GrantsClass.java
@@ -2,14 +2,9 @@
 public class GrantsClass {
 
 	public static void main(String[] args) {
-		for(int a = Integer.MIN_VALUE; a < Integer.MAX_VALUE; a++)
-		for(int b = Integer.MIN_VALUE; b < Integer.MAX_VALUE; b++)
-		for(int c = Integer.MIN_VALUE; c < Integer.MAX_VALUE; c++)
-		for(int d = Integer.MIN_VALUE; d < Integer.MAX_VALUE; d++)
-		for(int e = Integer.MIN_VALUE; e < Integer.MAX_VALUE; e++)
-		for(int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++)
-			System.out.println(i);
-		
+		for(int i = Integer.MAX_VALUE; i > 0; i--)
+			System.out.println("T minus "+i+" seconds");
+		System.out.println("BLAST OFF");
 
 	}
 

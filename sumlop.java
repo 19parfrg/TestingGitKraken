f02ace18d6b5a238a28068956a8a144bafa15a47
Unknown <ryan.p.osterberg@gmail.com>
2018-11-01 13:14:20 -0500

    sum gud lop

---
+++ sumlop.java
@@ -0,0 +1,13 @@
+
+public class sumlop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		while(i>10){
+			System.out.println("dis a gud progame!!!");
+			i--;
+		}
+	}
+
+}
